9b13aeee361a2606877d7fc749806011dcb5957d
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-16 15:59:32 +0800

    [Add] init

---
+++ Match/Controllers/MatchController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Match.Controllers;
+
+public class MatchController : Controller
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public MatchController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+}
